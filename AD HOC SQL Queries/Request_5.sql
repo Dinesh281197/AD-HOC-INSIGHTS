f015148ea2d1ca3d24336b7c8be528839db435f9
Dinesh Kumar S <s.dinesh.works@gmail.com>
2025-04-14 15:58:08 +0530

    Request_5.sql

--- AD HOC SQL Queries/Request_5.sql
+++ AD HOC SQL Queries/Request_5.sql
@@ -1,3 +1,8 @@
+/*5. Get the products that have the highest and lowest manufacturing costs.
+The final output should contain these fields,
+product_code
+product
+manufacturing_cost*/
 
 SELECT product,product_code, manufacturing_cost fROM fact_manufacturing_cost
 join dim_product
