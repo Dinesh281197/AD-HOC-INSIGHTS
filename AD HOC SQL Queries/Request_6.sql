045869943a4a016bd8d7feed91d72a50bc20a138
Dinesh Kumar S <s.dinesh.works@gmail.com>
2025-04-14 15:58:51 +0530

    Request_6.sql

--- AD HOC SQL Queries/Request_6.sql
+++ AD HOC SQL Queries/Request_6.sql
@@ -1,3 +1,11 @@
+/*6. Generate a report which contains the top 5 customers who received an
+average high pre_invoice_discount_pct for the fiscal year 2021 and in the
+Indian market. The final output contains these fields,
+customer_code
+customer
+average_discount_percentage*/
+
+
 SELECT customer_code, customer, round((avg(pre_invoice_discount_pct)*100),2) as avg_discount_pct FROM fact_pre_invoice_deductions
 join dim_customer
 using(customer_code)
