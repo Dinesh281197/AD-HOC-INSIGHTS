c4707fd32bd42bcd77ebe3e107dfb5829cfc1564
Dinesh Kumar S <s.dinesh.works@gmail.com>
2025-04-14 16:02:12 +0530

    Request_10.sql

--- AD HOC SQL Queries/Request_10.sql
+++ AD HOC SQL Queries/Request_10.sql
@@ -1,3 +1,13 @@
+/*10. Get the Top 3 products in each division that have a high
+total_sold_quantity in the fiscal_year 2021? The final output contains these
+fields,
+division
+product_code
+product
+total_sold_quantity
+rank_order*/ 
+
+
 with cte as(
 SELECT division, product_code, product,sum(sold_quantity) as total_sold_quantity FROM fact_sales_monthly
 join dim_product
