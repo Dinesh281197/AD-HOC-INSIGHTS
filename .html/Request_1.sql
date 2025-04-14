387616b5c12e7dc23fae708126258a4cfdf00823
Dinesh Kumar S <s.dinesh.works@gmail.com>
2025-04-14 15:40:17 +0530

    Delete .html directory

--- .html/Request_1.sql
+++
@@ -1,2 +0,0 @@
-SELECT market FROM dim_customer 
- where customer like '%Atliq Exclusive%' and region='APAC'
--- .html/Request_7.sql
+++
@@ -1,9 +0,0 @@
-SELECT monthname(date) as month,year(date) as year,concat((round((sum(sold_quantity*gross_price)/1000000),2)),"M")as Gross_sales_Amount_mln
-FROM fact_sales_monthly s
-JOIN fact_gross_price g
-using(product_code)
-join dim_customer c
-using(customer_code)
-where customer = "Atliq Exclusive"
-group by date
-order by date
