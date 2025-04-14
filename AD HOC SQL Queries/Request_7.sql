e2e0f2fdd7d34da4baa033b8fc4f1c6ff79d0471
Dinesh Kumar S <s.dinesh.works@gmail.com>
2025-04-14 15:59:38 +0530

    Request_7.sql

--- AD HOC SQL Queries/Request_7.sql
+++ AD HOC SQL Queries/Request_7.sql
@@ -1,3 +1,11 @@
+/*7. Get the complete report of the Gross sales amount for the customer “Atliq
+Exclusive” for each month. This analysis helps to get an idea of low and
+high-performing months and take strategic decisions.
+The final report contains these columns:
+Month
+Year
+Gross sales Amount*/
+
 SELECT monthname(date) as month,year(date) as year,concat((round((sum(sold_quantity*gross_price)/1000000),2)),"M")as Gross_sales_Amount_mln
 FROM fact_sales_monthly s
 JOIN fact_gross_price g
