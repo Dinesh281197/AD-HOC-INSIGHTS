babdaf4151ece6a40ea85e9db76c8e6576694680
Dinesh Kumar S <s.dinesh.works@gmail.com>
2025-04-14 15:51:49 +0530

    Delete SQL Queries directory

--- SQL Queries/Request_6.sql
+++
@@ -1,7 +0,0 @@
-SELECT customer_code, customer, round((avg(pre_invoice_discount_pct)*100),2) as avg_discount_pct FROM fact_pre_invoice_deductions
-join dim_customer
-using(customer_code)
-where fiscal_year=2021 and market like "%india%"
-group by customer_code,customer
-order by avg_discount_pct desc
-limit 5;
