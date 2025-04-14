ac0e3d26a77b9d60d95de17a6e88647c4701e725
Dinesh Kumar S <s.dinesh.works@gmail.com>
2025-04-14 16:01:47 +0530

    Request_9.sql

--- AD HOC SQL Queries/Request_9.sql
+++ AD HOC SQL Queries/Request_9.sql
@@ -1,3 +1,9 @@
+/*9. Which channel helped to bring more gross sales in the fiscal year 2021
+and the percentage of contribution? The final output contains these fields,
+channel
+gross_sales_mln
+percentage*/
+
 with cte_1 as(
 SELECT channel,
 concat((round((sum(sold_quantity*gross_price)/1000000),2)),"M")as Gross_sales_Amount_mln
