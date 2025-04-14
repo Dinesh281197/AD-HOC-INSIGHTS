5fdcd87d4d67f5c12a27c3affd56247995386405
Dinesh Kumar S <s.dinesh.works@gmail.com>
2025-04-14 15:55:48 +0530

    Request_1.sql

--- AD HOC SQL Queries/Request_1.sql
+++ AD HOC SQL Queries/Request_1.sql
@@ -1,2 +1,5 @@
-SELECT market FROM dim_customer 
+/*1. Provide the list of markets in which customer "Atliq Exclusive" operates its
+business in the APAC region.*/
+
+SELECT distinct market FROM dim_customer 
  where customer like '%Atliq Exclusive%' and region='APAC'
